9915120f02c4dc154c5d5f231a2f9f6fb6c018b3
Chunkamunk <christian.rickert.1989@gmail.com>
2025-06-24 12:45:58 +0200

    Add dependencies and setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trading_bot',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'ccxt',
+        'pandas',
+        'python-dotenv',
+        'pyarrow',
+        'gspread',
+        'oauth2client',
+        'numpy',
+        'python-telegram-bot',
+        'tenacity',
+        'pytest',
+        'pytest-mock'
+    ],
+    author='Christian Rickert',
+    description='A cryptocurrency trading bot using Bitvavo API',
+    license='MIT',
+)
